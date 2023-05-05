1f2e3f757a91c32d2e7b4f42b2a6df9db284410e
Forever.Trexie <68247169+bunnytrex@users.noreply.github.com>
2023-05-05 20:14:21 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,9 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		console.log(`Operational! Logged in as ${client.user.tag}!`);
+		console.log(`Successfully logged in as ${client.user.tag}!`);
         client.user.setStatus('online'); // online/dnd/invisible/idle
         const { ActivityType } = require('discord.js');
-        client.user.setActivity('Watching over CostaBlox.', { type: ActivityType.Watching }); //Listening/Watching/Competing
+        client.user.setActivity('Activity / What it is doing', { type: ActivityType.Watching }); //Listening/Watching/Competing
 	},
 };
