5059cbac0ecee776f99c8bd17cf0c0c66421e8a5
Forever.Trexie <68247169+bunnytrex@users.noreply.github.com>
2023-05-05 20:13:00 +0100

    Update and rename none-embed-command.js to non-embed-command.js

--- commands/non-embed-command.js
+++ commands/non-embed-command.js
@@ -2,10 +2,11 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('none-embed')
-        .setDescription('Comand Description'),
+        .setName('non-embed')
+        .setDescription('Command Description'),
     async execute(interaction, client) {
       // You can add functions
+        
             return interaction.reply({ content: `Text`, ephemeral: false });
         }
     },
