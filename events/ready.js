eaa5dc69152bbac647623918c9b1659242df4613
Forever.Trexie <68247169+bunnytrex@users.noreply.github.com>
2023-05-05 20:10:15 +0100

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+	name: 'ready',
+	once: true,
+	execute(client) {
+		console.log(`Operational! Logged in as ${client.user.tag}!`);
+        client.user.setStatus('online'); // online/dnd/invisible/idle
+        const { ActivityType } = require('discord.js');
+        client.user.setActivity('Watching over CostaBlox.', { type: ActivityType.Watching }); //Listening/Watching/Competing
+	},
+};
